ac47a883de8fa62953bbc400176d85dafc17c102
HexoPuss <vackas1212@gmail.com>
2024-12-18 23:31:58 +0300

    навигация по страничкам

--- terrainscan/analysis/views.py
+++ terrainscan/analysis/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def analysis_page(request):
+    return HttpResponse("<h4>тут должна быть страничка с анализом снимка<h4>")
